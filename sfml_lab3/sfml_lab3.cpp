b15521c8553bd21e0b4c8f62add479315754251b
anastasiaskbb <anastasiaskbb@mail.ru>
2020-12-22 17:05:35 +0400

    laba3

---
+++ sfml_lab3/sfml_lab3.cpp
@@ -0,0 +1,249 @@
+﻿#include <SFML/Graphics.hpp>
+using namespace sf;
+#include "map.h" //подключили код с картой
+#include "view.h"
+#include <sstream>
+
+class Player { // класс Игрока
+private: float x, y;//объявили переменные, в конструкторе Player ниже дадим им значения
+public:
+	float w, h, dx, dy, speed;
+	int dir, playerScore, health;//новая переменная, хранящая жизни игрока
+	bool life;//переменная жизнь, логическая
+	String File;
+	Image image;
+	Texture texture;
+	Sprite sprite;
+	Player(String F, float X, float Y, float W, float H) {
+		dir = 0; speed = 0; playerScore = 0; health = 100; // инициализировали переменную жизни в конструкторе
+		life = true;//инициализировали логическую переменную жизни
+		File = F;//имя файла+расширение
+		w = W; h = H;//высота и ширина
+		image.loadFromFile("images/" + File);//запихиваем в image наше изображение вместо File мы передадим то, что пропишем при создании объекта. В нашем случае "hero.png" и получится запись идентичная image.loadFromFile("images/hero/png");
+		image.createMaskFromColor(Color(41, 33, 59));//убираем ненужный темно-синий цвет
+		texture.loadFromImage(image);//закидываем наше изображение в текстуру
+		sprite.setTexture(texture);//заливаем спрайт текстурой
+		x = X; y = Y;//координата появления спрайта
+		sprite.setTextureRect(IntRect(0, 0, w, h)); //Задаем спрайту один прямоугольник для вывода одного льва, а не кучи львов сразу. IntRect - приведение типов
+	}
+
+	void update(float time) // принимает в себя время SFML , вследствие чего работает бесконечно, давая персонажу движение.
+	{
+		switch (dir)//реализуем поведение в зависимости от направления. (каждая цифра соответствует направлению)
+		{
+		case 0: dx = speed; dy = 0; break;//по иксу задаем положительную скорость, по игреку зануляем. получаем, что персонаж идет только вправо
+		case 1: dx = -speed; dy = 0; break;//по иксу задаем отрицательную скорость, по игреку зануляем. получается, что персонаж идет только влево
+		case 2: dx = 0; dy = speed; break;//по иксу задаем нулевое значение, по игреку положительное. получается, что персонаж идет только вниз
+		case 3: dx = 0; dy = -speed; break;//по иксу задаем нулевое значение, по игреку отрицательное. получается, что персонаж идет только вверх
+		}
+
+		x += dx * time;// наше ускорение на время получаем смещение координат и как следствие движение
+		y += dy * time;//аналогично по игреку
+
+		speed = 0;//зануляем скорость, чтобы персонаж остановился.
+		sprite.setPosition(x, y); //выводим спрайт в позицию x y , посередине. бесконечно выводим в этой функции, иначе бы наш спрайт стоял на месте.
+
+		interactionWithMap();//вызываем функцию, отвечающую за взаимодействие с картой
+
+		
+	}
+
+	
+	float getPlayerCoordinateX()
+	{	//этим методом будем забирать координату Х	
+		return x;
+	}
+	float getPlayerCoordinateY() 
+	{	//этим методом будем забирать координату Y 	
+		return y;
+	}
+
+	void interactionWithMap()//ф-ция взаимодействия с картой
+	{
+
+		for (int i = y / 32; i < (y + h) / 32; i++)//проходимся по тайликам, контактирующим с игроком, то есть по всем квадратикам размера 32*32, которые мы окрашивали в 9 уроке. про условия читайте ниже.
+			for (int j = x / 32; j < (x + w) / 32; j++)//икс делим на 32, тем самым получаем левый квадратик, с которым персонаж соприкасается. (он ведь больше размера 32*32, поэтому может одновременно стоять на нескольких квадратах). А j<(x + w) / 32 - условие ограничения координат по иксу. то есть координата самого правого квадрата, который соприкасается с персонажем. таким образом идем в цикле слева направо по иксу, проходя по от левого квадрата (соприкасающегося с героем), до правого квадрата (соприкасающегося с героем)
+			{
+				if (TileMap[i][j] == '0')//если наш квадратик соответствует символу 0 (стена), то проверяем "направление скорости" персонажа:
+				{
+					if (dy > 0)//если мы шли вниз,
+					{
+						y = i * 32 - h;//то стопорим координату игрек персонажа. сначала получаем координату нашего квадратика на карте(стены) и затем вычитаем из высоты спрайта персонажа.
+					}
+					if (dy < 0)
+					{
+						y = i * 32 + 32;//аналогично с ходьбой вверх. dy<0, значит мы идем вверх (вспоминаем координаты паинта)
+					}
+					if (dx > 0)
+					{
+						x = j * 32 - w;//если идем вправо, то координата Х равна стена (символ 0) минус ширина персонажа
+					}
+					if (dx < 0)
+					{
+						x = j * 32 + 32;//аналогично идем влево
+					}
+				}
+
+				if (TileMap[i][j] == 's') {
+					playerScore++;//если взяли камень, переменная playerScore=playerScore+1;
+					TileMap[i][j] = ' ';
+				}
+
+				if (TileMap[i][j] == 'f') {
+					health -= 40;//если взяли ядовитейший в мире цветок,то переменная health=health-40;
+					TileMap[i][j] = ' ';//убрали цветок
+				}
+
+				if (TileMap[i][j] == 'h') {
+					health += 20;//если взяли сердечко,то переменная health=health+20;
+					TileMap[i][j] = ' ';//убрали сердечко
+				}
+			}
+	}
+};
+
+int main()
+
+{
+
+	RenderWindow window(VideoMode(640, 480), "Game");
+
+	view.reset(sf::FloatRect(0, 0, 640, 480));//размер "вида" камеры при создании объекта вида камеры.
+
+	Font font;//шрифт 
+	font.loadFromFile("CyrilicOld.TTF");//передаем нашему шрифту файл шрифта
+	Text text("", font, 25);//создаем объект текст. закидываем в объект текст строку, шрифт, размер шрифта(в пикселях);//сам объект текст (не строка)
+	text.setColor(Color::White);//покрасили текст в красный. если убрать эту строку, то по умолчанию он белый
+	text.setStyle(Text::Bold);//жирный текст.
+
+	
+	Text text1("", font, 50);//создаем объект текст. закидываем в объект текст строку, шрифт, размер шрифта(в пикселях);//сам объект текст (не строка)
+	text1.setColor(Color::White);//покрасили текст в красный. если убрать эту строку, то по умолчанию он белый
+	text1.setStyle(Text::Bold);//жирный текст.
+	
+
+	Image map_image;//объект изображения для карты
+	map_image.loadFromFile("images/map.png");//загружаем файл для карты
+	Texture map;//текстура карты
+	map.loadFromImage(map_image);//заряжаем текстуру картинкой
+	Sprite s_map;//создаём спрайт для карты
+	s_map.setTexture(map);//заливаем текстуру спрайтом
+
+	Image heroimage; //создание объекта Image
+	heroimage.loadFromFile("Images/hero.png"); //загружаем файл в объект
+
+
+	Player p("hero.png", 250, 250, 96.0, 96.0);//создаем объект p класса player,задаем "hero.png" как имя файла+расширение, далее координата Х,У, ширина, высота.
+
+	float CurrentFrame = 0;//хранит текущий кадр
+	Clock clock;
+	Clock gameTimeClock;//переменная игрового времени, будем здесь хранить время игры 
+	int gameTime = 0;//объявили игровое время, инициализировали.
+	while (window.isOpen())
+	{
+
+		if (p.health <= 0)
+		{
+
+			p.life = false;
+			p.speed = 0;
+			text1.setString("Игра окончена :(");//задает строку тексту
+			text1.setPosition(view.getCenter().x - 165, view.getCenter().y - 50);
+			
+		}
+
+		float time = clock.getElapsedTime().asMicroseconds();
+
+		if (p.life) gameTime = gameTimeClock.getElapsedTime().asSeconds(); //игровое время в секундах идёт вперед, пока жив игрок, перезагружать как time его не надо. оно не обновляет логику игры
+		clock.restart();
+		time = time / 800;
+
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		
+
+		if (p.life) 
+		{
+
+			if (Keyboard::isKeyPressed(Keyboard::Left)) {
+				p.dir = 1; p.speed = 0.1;
+				CurrentFrame += 0.005 * time;
+				if (CurrentFrame > 3) CurrentFrame -= 3;
+				p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 96, 96, 96));
+				
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::Right)) {
+				p.dir = 0; p.speed = 0.1;
+				CurrentFrame += 0.005 * time;
+				if (CurrentFrame > 3) CurrentFrame -= 3;
+				p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 192, 96, 96));
+				
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::Up)) {
+				p.dir = 3; p.speed = 0.1;
+				CurrentFrame += 0.005 * time;
+				if (CurrentFrame > 3) CurrentFrame -= 3;
+				p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 307, 96, 96));
+				
+
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::Down)) {
+				p.dir = 2; p.speed = 0.1;
+				CurrentFrame += 0.005 * time;
+				if (CurrentFrame > 3) CurrentFrame -= 3;
+				p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
+
+			}
+
+			getplayercoordinateforview(p.getPlayerCoordinateX(), p.getPlayerCoordinateY());
+		}
+
+		
+		
+
+		p.update(time);//оживляем объект p класса Player с помощью времени sfml, передавая время в качестве параметра функции update. благодаря этому персонаж может двигаться
+		window.setView(view);//"оживляем" камеру в окне sfml
+		window.clear();
+
+		/////////////////////////////Рисуем карту/////////////////////
+		for (int i = 0; i < HEIGHT_MAP; i++)
+			for (int j = 0; j < WIDTH_MAP; j++)
+			{
+				if (TileMap[i][j] == ' ')  s_map.setTextureRect(IntRect(0, 0, 32, 32)); //если встретили символ пробел, то рисуем 1й квадратик
+				if (TileMap[i][j] == 's')  s_map.setTextureRect(IntRect(32, 0, 32, 32));//если встретили символ s, то рисуем 2й квадратик
+				if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(64, 0, 32, 32));//если встретили символ 0, то рисуем 3й квадратик
+				if ((TileMap[i][j] == 'f')) s_map.setTextureRect(IntRect(96, 0, 32, 32));//добавили цветок
+				if ((TileMap[i][j] == 'h')) s_map.setTextureRect(IntRect(128, 0, 32, 32));//и сердечко
+
+
+				s_map.setPosition(j * 32, i * 32);//по сути раскидывает квадратики, превращая в карту. то есть задает каждому из них позицию. если убрать, то вся карта нарисуется в одном квадрате 32*32 и мы увидим один квадрат
+
+				window.draw(s_map);//рисуем квадратики на экран
+			}
+
+		
+		std::ostringstream playerHealthString, gameTimeString, playerScoreString;    // объявили переменную playerScoreString
+		playerScoreString << p.playerScore;;    // объявили переменную здоровья и времени
+		playerHealthString << p.health; gameTimeString << gameTime;		//формируем строку
+		text.setString("Собрано камней:" + playerScoreString.str() + "\nЗдоровье: " + playerHealthString.str() + "\nВремя игры: " + gameTimeString.str());//задаем строку тексту и вызываем сформированную выше строку методом .str()
+
+		text.setPosition(view.getCenter().x - 165, view.getCenter().y - 200);//задаем позицию текста, отступая от центра камеры
+		window.draw(text);//рисую этот текст
+		window.draw(text1);
+		
+		window.draw(p.sprite);//рисуем спрайт объекта p класса player
+		window.display();
+
+	}
+
+	return 0;
+
+}
